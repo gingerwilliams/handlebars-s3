94c962a1c4713bb2ecc33bb128eca2c27d33f469
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2017-11-14 11:33:53 -0500

    serverless routing

--- index.js
+++ index.js
@@ -10,6 +10,49 @@ function getParameterByName(name, url) {
 }
 
 
+var template = document.getElementById('handlebars-demo').innerHTML;
+
+var templateScript = Handlebars.compile(template);
+
+$.getJSON("data.json", function(data){
+    
+}
+
+// handlebars.registerHelper("printComponents", function(block) {
+//     var components = "Components";
+//     var item = '<div>';
+
+//     for(var i=0; i < data.length; i++ ){
+//         var id = data.indexOf(data[i]);
+//         var ref = data[i].reference;
+//         var link = ref.substr(ref.indexOf('.')+1);
+        
+//         if(ref.includes(components) === true){
+            
+//             item += '<li class="navigation_menu-item"><a class="navigation_menu-link" href="/components/' + id + '">' + link + '</a></li>';
+//         }
+//     }
+//     return item += '</div>';
+// });
+
+// handlebars.registerHelper("printMarkup", function(block) {
+//     var markupblocks = "Markup Blocks";
+//     var item = '<div>';
+
+//     for(var i=0; i < data.length; i++ ){
+//         var id = data.indexOf(data[i]);
+//         var ref = data[i].reference;
+//         var link = ref.substr(ref.indexOf('.')+1);
+        
+//         if(ref.includes(markupblocks) === true){
+            
+//             item += '<li class="navigation_menu-item"><a class="navigation_menu-link" href="/markupblocks/' + id + '">' + link + '</a></li>';
+//         }
+//     }
+//     return item += '</div>';
+// });
+
+
 //Partials
 // Handlebars.registerPartial(
 //     'navPartial', 
